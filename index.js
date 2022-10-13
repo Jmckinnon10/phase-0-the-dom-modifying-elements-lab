efd06e37fe727218f140a597955767589ddb67d4
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-13 09:37:26 -0700

    commit tree cf1faffee3d83d4cad89a419f15a37588982e655

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+
+const main = document.querySelector('#main')
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = `JOHN is the champion`
+let name;
+
+
+
+
+const element = document.createElement("div");
+
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
